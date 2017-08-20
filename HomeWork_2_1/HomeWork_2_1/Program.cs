6517b653efda01bb39ad96a78be32ce22f660ead
letkovladislav <letkovladislav@gmail.com>
2017-08-20 19:25:22 +0300

    HomeWork_2_!

--- HomeWork_2_1/HomeWork_2_1/Program.cs
+++ HomeWork_2_1/HomeWork_2_1/Program.cs
@@ -11,6 +11,41 @@ namespace HomeWork_2_1
         static void Main(string[] args)
         {
 
+            double numInput_1;
+            double numInput_2;
+           
+            Console.Write(" Hallo, to add two numbers, enter the first number: ");
+            numInput_1 = double.Parse(Console.ReadLine());
+            Console.Write(" Enter the second number: ");
+            numInput_2 = double.Parse(Console.ReadLine());
+            Console.WriteLine(" Result: " + (numInput_1 + numInput_2));
+
+            Console.Write(" You can subtract from one number another, for this, enter the first number: ");
+            numInput_1 = double.Parse(Console.ReadLine());
+            Console.Write(" Enter the second number: ");
+            numInput_2 = double.Parse(Console.ReadLine());
+            Console.WriteLine(" Result: " + (numInput_1 - numInput_2));
+
+            Console.Write(" You can multiply one number by another, to do this, enter the first number: ");
+            numInput_1 = double.Parse(Console.ReadLine());
+            Console.Write(" Enter the second number: ");
+            numInput_2 = double.Parse(Console.ReadLine());
+            Console.WriteLine(" Result: " + (numInput_1 * numInput_2));
+
+            Console.Write(" You can divide one number into another, for this, enter the first number: ");
+            numInput_1 = double.Parse(Console.ReadLine());
+            Console.Write(" Enter the second number: ");
+            numInput_2 = double.Parse(Console.ReadLine());
+            Console.WriteLine(" Result: " + (numInput_1 / numInput_2));
+
+            Console.Write(" You can calculate the remainder of the division for the number: ");
+            numInput_1 = double.Parse(Console.ReadLine());
+            Console.Write(" Divide by number: ");
+            numInput_2 = double.Parse(Console.ReadLine());
+            Console.WriteLine(" Result: " + (numInput_1 % numInput_2));
+
+            Console.Write(" Thank you! Game over!");
+
             Console.ReadLine();
         }
     }
