5fdb08893b4a3f7a92031d366cb7ebfd959907be
letkovladislav <letkovladislav@gmail.com>
2017-08-21 23:23:15 +0300

    c

---
+++ HomeWork_2_2/HomeWork_2_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 4;
+            double b = 5;
+            double y = (a / b);
+            double z = 20;
+            double x = (z / y);
+
+            Console.WriteLine("Solve equation: 4/5 * x - 20 = 0 ");
+            Console.WriteLine("x =  " + x );
+
+            Console.ReadLine();
+        }
+    }
+}
