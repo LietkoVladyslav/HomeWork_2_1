254e0861ad3f571bd09006cdbbaf956720924213
letkovladislav <letkovladislav@gmail.com>
2017-08-25 20:20:26 +0300

    H

---
+++ HW_4_5/HW_4_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hiPrice = 2.0;
+            double lowPrice = 1.0;
+            double newPrice;
+
+            Console.WriteLine("Enter new price: ");
+            newPrice = double.Parse(Console.ReadLine());
+            
+            if (newPrice > hiPrice)
+            {
+                Console.WriteLine("Price has a new hi level ");
+            }
+            if  (newPrice < lowPrice)
+            {
+                Console.WriteLine("Price has a new low level ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
