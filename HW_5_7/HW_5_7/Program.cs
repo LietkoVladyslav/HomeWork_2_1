c800d361826e1e7b4ef580403f8a61495e4a2e77
letkovladislav <letkovladislav@gmail.com>
2017-08-26 00:45:56 +0300

    HW_5.7

---
+++ HW_5_7/HW_5_7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_5_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            double n;
+
+            Console.WriteLine("Hallo! This programm will calculates sum from 1 to N. Please enter the N:  ");
+            n = double.Parse(Console.ReadLine());
+
+
+            while (a <= n) 
+            {
+                
+                Console.WriteLine(a++);
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
